4a1fd2258dbe1109e7d252b0f1347b520c860681
HakSoo Ji <MintZzz1009@github.com>
2022-07-02 13:20:09 +0900

    Add new branch "HakSoo"

--- src/main/java/com/example/javastudy/MainApplication.java
+++ src/main/java/com/example/javastudy/MainApplication.java
@@ -4,6 +4,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("test");
-        //지학수 브런치
+        //HakSoo addBranch 
     }
 }
